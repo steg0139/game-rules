955bfab74e78a12627f512d8eef155652e7ecd76
Josh Stegman <jsteg21@gmail.com>
2021-07-15 11:10:02 -0500

    getting some rules ready

--- src/components/HandFootRules.js
+++ src/components/HandFootRules.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faHandPointRight} from "@fortawesome/free-solid-svg-icons";
 
 const HandFootRules = () => {
+  const rules = [{title: 'Hello', subRules: ['Dogs', 'Cats']},
+    {title: 'Goodbye', subRules: ['frogs', 'kids']},
+    {title: 'Alligator', subRules: ['brokles', 'turtles']}]
+
   return (
       <React.Fragment>
-        {(
-            <div className="hand-foot-rules">
-              Hello albums
-            </div>
-        )}
+        {rules.map((rule, index) => {
+          return (
+              <React.Fragment key={index}>
+                <div className="hand-foot-rules">
+                  <div className="rules-subsection">
+                    <FontAwesomeIcon className="rule-icon" icon={faHandPointRight}/>{rule.title}
+                  </div>
+                </div>
+              </React.Fragment>
+          );
+        })}
       </React.Fragment>
   );
 };
